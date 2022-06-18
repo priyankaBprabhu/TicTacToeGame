1c2c0fb484d524cc9b947ac3f4f6cbdcd9bc7aa9
priyankaBprabhu <priyankaprabhu96@gmail.com>
2022-06-18 20:40:46 +0530

    Added code for toss to check who play first

--- src/com/bridgelabz/TicTacToeMain.java
+++ src/com/bridgelabz/TicTacToeMain.java
@@ -4,14 +4,19 @@ import static com.bridgelabz.TicTacToeImpl.scanner;
 public class TicTacToeMain {
     public static char userTurn;
     public static char computerTurn;
+    public static int chance;
     public static void main(String[] args) {
         System.out.println("tic tac toe game");
         TicTacToeImpl.toeBoard();
-        TicTacToeImpl.showBoard();
-        userTurn = TicTacToeImpl.user(scanner);
-        computerTurn = (userTurn == 'X')? 'O' :  'X';
-        TicTacToeImpl.moveLocation(userTurn);
-        TicTacToeImpl.comLocation(computerTurn);
+        chance = (int) (Math.random() * 10 % 2) ;
+        if(chance == 0) {
+            userTurn = TicTacToeImpl.user(scanner);
+            TicTacToeImpl.moveLocation(userTurn);
+        }
+        else {
+            computerTurn = (userTurn == 'X')? 'O' :  'X';
+            TicTacToeImpl.comLocation(computerTurn);
+        }
         TicTacToeImpl.showBoard();
     }
 }

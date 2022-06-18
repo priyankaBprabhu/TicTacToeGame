9e1aaf02bea97ccb644fdc000886bbad433f4fb5
priyankaBprabhu <priyankaprabhu96@gmail.com>
2022-06-18 17:44:29 +0530

    Added code to display the current game board to the player

--- src/com/bridgelabz/TicTacToeMain.java
+++ src/com/bridgelabz/TicTacToeMain.java
@@ -9,6 +9,8 @@ public class TicTacToeMain {
     public static void main(String[] args) {
         System.out.println("tic tac toe game");
         TicTacToeImpl ticTacToe = new TicTacToeImpl();
+        ticTacToe.toeBoard();
+        ticTacToe.showBoard();
         userTurn = ticTacToe.user(scanner);
         computerTurn =  ((userTurn == 'X') ? 'O' : 'X');
 
